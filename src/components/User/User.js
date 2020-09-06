d08727d63b5acae006b5a82ef235b0fdddb45572
al-amin-khan <alaminkhan2010@gmail.com>
2020-09-06 23:21:13 +0600

    card added in user.js

--- src/components/User/User.js
+++ src/components/User/User.js
@@ -1,16 +1,51 @@
 import React from 'react';
 import Container from '@material-ui/core/container';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+
+const useStyles = makeStyles({
+  root: {
+    maxWidth: 345,
+  },
+});
 
 
 
 
 
 const User = (props) => {
+  const classes = useStyles();
+
   const {id, title, body} = props.user;
 
   return (<div>
     <Container maxWidth="md">
-
+      <Card className={classes.root}>
+      <CardActionArea>
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+            {title}
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+            {body}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        <Button size="small" color="primary">
+          Share
+        </Button>
+        <Button size="small" color="primary">
+          Learn More
+        </Button>
+      </CardActions>
+    </Card>
     </Container>
   </div>);
 }
