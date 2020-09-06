c8a21d85bc704e796e698b856e2463e48516c024
al-amin-khan <alaminkhan2010@gmail.com>
2020-09-06 23:09:09 +0600

    incomplate user component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {useState, useEffect} from 'react'
+import User from './components/User/User'
 
 function App() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    const url = 'https://jsonplaceholder.typicode.com/posts';
+    fetch(url)
+    .then(response => response.json())
+    .then(data => setUsers(data))
+  },[])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {
+        users.map(user => <User user={user}></User>)
+      }
     </div>
   );
 }
---
+++ src/components/User/User.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Container from '@material-ui/core/container';
+
+
+
+
+
+const User = (props) => {
+  const {id, title, body} = props.user;
+
+  return (<div>
+    <Container maxWidth="md">
+
+    </Container>
+  </div>);
+}
+
+export default User;
